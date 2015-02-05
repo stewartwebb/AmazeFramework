947cda2d69ce0a4bc947d57f77ccfd6246c3844f
Stewart Webb <hello@stewartwebb.co.uk>
2015-02-04 16:09:58 -0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -11,5 +11,5 @@ define('BASE_URL', WEB_HOST . WEB_APP_NAME);
 
 define('DB_NAME', 'wightbread');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'RangersAreGo');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
